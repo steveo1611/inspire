3981b1556fa51ab9e894594ee070f6e33b21ea24
Steven Oldfield <steveold1611@gmail.com>
2018-09-15 19:30:05 -0600

    more css-grid fun

--- app/components/todo/todo-controller.js
+++ app/components/todo/todo-controller.js
@@ -1,11 +1,5 @@
 function TodoController() {
-	// new up the TodoService that has already been configured for your use
-	// You will need four methods
-	// getTodos should request your api/todos and give an array of todos to your callback fn
-	// addTodo takes in a todo and posts it to the server
-	// toggleTodoStatus takes in a todo marks its status as completed and puts it to the server
-	// removeTodo takes in a todoId and sends a delete request to the server
-	// **** HINT: Everytime you make a change to any todo don't forget to get the todo list again
+
 	var todoService = new TodoService()
 
 	// Use this getTodos function as your callback for all other edits
